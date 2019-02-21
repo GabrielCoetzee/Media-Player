339e68b2b1e9a26b886dc6ec5a313e7062b6d7a7
André Gabriel Coetzee <andre.gabriel.coetzee@gmail.com>
2019-02-21 19:01:45 +0200

    Solution Clean-up
    - Solution Clean-up

--- MediaPlayer.View/Properties/AssemblyInfo.cs
+++ MediaPlayer.View/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MediaPlayer")]
+[assembly: AssemblyTitle("MediaPlayer.View")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("rg-adguard")]
-[assembly: AssemblyProduct("MediaPlayer")]
+[assembly: AssemblyProduct("MediaPlayer.View")]
 [assembly: AssemblyCopyright("Copyright © rg-adguard 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
